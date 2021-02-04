7504ce318591f40e189e716b76d4a900ef17ad1c
Greg Ottley <greg.ottley@e4education.co.uk>
2021-02-04 10:10:23 +0000

    Remove welcome email on register as it clashes with verification email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\Mail;
-use App\Mail\NewUserWelcomeMail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -51,10 +49,6 @@ class User extends Authenticatable implements MustVerifyEmail
             $user->profile()->create([
                 'title' => $user->username,
             ]);
-
-            // send email on register
-
-            Mail::to($user->email)->send(new NewUserWelcomeMail());
         });
     }
 
